ac6fdde9cc89a33e3083b0366349ed45a1a2d7b7
Cosmo André <soucosmomoraes@gmail.com>
2024-06-30 19:39:03 -0300

    Novos testes

--- src/btc_utils/create_public_hash160.rs
+++ src/btc_utils/create_public_hash160.rs
@@ -1,9 +1,10 @@
 use secp256k1::{Secp256k1, SecretKey};
 use super::{decode_hex, hash160};
 use std::num::ParseIntError;
-
+use super::generate_wif;
 
 pub fn create_public_hash160(priv_key_int: &str) -> Result<Vec<u8>, ParseIntError> {
+    println!("priv_key_int: {}", priv_key_int);
     let priv_key_bytes = decode_hex(priv_key_int)?;
 
     // Cria uma nova chave privada usando o pacote secp256k1
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod btc_utils;
 mod base58;
 mod wallet;
 mod wallets;
-
+use crate::base58::encode;
 use btc_utils::{generate_wif, create_public_hash160};
 
 
@@ -16,8 +16,32 @@ fn main() {
         Err(e) => println!("Erro ao gerar WIF: {}", e),
     }
 
+    let addr = "1CUNEBjYrCn2y1SdiUMohaKUi4wpP326Lb";
+
+    let pke = [2, 249, 48, 138, 1, 146, 88, 195, 16, 73, 52, 79, 133, 248, 157, 82, 41, 181, 49, 200, 69, 131, 111, 153, 176, 134, 1, 241, 19, 188, 224, 54, 249];
+
     match create_public_hash160(priv_key_int) {
-        Ok(pub_hash) => println!("Public Hash160: {:?}", pub_hash),
+        Ok(pub_hash) => {
+            println!("Public Hash160: {:?}", pub_hash);
+
+            let a = encode(&pub_hash);
+            println!("a: {}", a);
+            for i in &pke {
+                //print!("{:02x}", i);
+                match generate_wif(format!("{:02x}", i).as_str()) {
+                    Ok(wif) => {
+                        //println!(" WIF: {}", &wif);
+
+                        if addr.contains(&wif) {
+                            println!("encontrou  a chave");
+                        } else {
+                            println!("nao encontrou a chave");
+                        }
+                    },
+                    Err(e) => println!("Erro ao gerar WIF: {}", e),
+                }
+            }
+        },
         Err(e) => println!("Erro na criação da public hash160: {}", e),
     }
 }
