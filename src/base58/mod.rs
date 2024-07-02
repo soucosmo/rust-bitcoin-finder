3783b1d6f9416146414db171b91070380cad3405
Cosmo André <cosmo@ecwsa.com.br>
2024-07-02 18:52:14 -0300

    otimizado tempo de soma da próxima chave hexadecimal e removido módulo base58 sem uso

--- src/base58/mod.rs
+++
@@ -1,4 +0,0 @@
-mod decode;
-mod encode;
-
-pub const BASE58_ALPHABET: &[u8] = b"123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
--- src/btc_utils/mod.rs
+++ src/btc_utils/mod.rs
@@ -1,5 +1,4 @@
 mod private_key_to_public_key;
-mod increment_and_format_key;
 mod public_key_to_address;
 mod create_public_hash160;
 mod generate_wif;
@@ -8,7 +7,6 @@ mod hash160;
 mod sha256;
 
 pub use private_key_to_public_key::private_key_to_public_key;
-pub use increment_and_format_key::increment_and_format_key;
 pub use public_key_to_address::public_key_to_address;
 pub use generate_wif::generate_wif;
 pub use decode_hex::decode_hex;
