8160f5757f8a6ca2631d766236259a1e6a1bb159
Cosmo André <soucosmomoraes@gmail.com>
2024-07-15 02:49:46 -0300

    otimizado processo de padding de bytes com alocação estática de 32 bytes

--- src/utils/verify_address_with_murmur.rs
+++ src/utils/verify_address_with_murmur.rs
@@ -1,8 +1,10 @@
 use super::{generate_public_key, hash160, murmur_hash};
+use secp256k1::{Context, Secp256k1};
 use num_bigint::BigUint;
 
-pub fn verify_address_with_murmur(priv_key_int: &BigUint, target_hash: u32) -> bool {
-    let pub_key = generate_public_key(priv_key_int);
+
+pub fn verify_address_with_murmur<C: Context + secp256k1::Signing>(priv_key_int: &BigUint, target_hash: u32, secp: &Secp256k1<C>) -> bool {
+    let pub_key = generate_public_key(priv_key_int, secp);
     let pub_key_hash160 = hash160(&pub_key);
     let pub_key_murmur = murmur_hash(&pub_key_hash160);
 
